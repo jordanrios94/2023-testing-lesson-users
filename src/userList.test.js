002b5b6fc0e1a85931b45220e2c8b6d256c31827
jordanrios94 <jordanrios.software@gmail.com>
2023-06-13 14:25:06 +0100

    test:  using dataid to select elements for testing

---
+++ src/userList.test.js
@@ -0,0 +1,27 @@
+import { render, screen, within } from '@testing-library/react';
+import UserList from './UserList';
+
+test('render one row per user', () => {
+  // Render the component
+  const users = [
+    {
+      name: 'jane',
+      email: 'jane@yahoo.com',
+    },
+    {
+      name: 'sam',
+      email: 'sam@gmail.com',
+    },
+  ];
+  render(<UserList users={users} />);
+
+  // Find all the rows in the table
+  const rows = within(screen.getByTestId('users')).getAllByRole(
+    'row'
+  );
+
+  // Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
+});
+
+test('render the email and name of each user', () => {});
